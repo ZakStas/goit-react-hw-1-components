a55b3f5cd73ffb5196fdb1560f89858b7d9b7907
ZakStas <stanislavzakarliuka1989@gmail.com>
2020-04-05 18:41:09 +0300

    Updates

---
+++ precache-manifest.2094ec8b48ef2f0f6ddfc288bc265005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec0145f0737f1f980dfe3b373bcda8e",
+    "url": "/goit-react-hw-1-components/index.html"
+  },
+  {
+    "revision": "78fe0d596fb8da2fb3b0",
+    "url": "/goit-react-hw-1-components/static/css/main.d9b7e441.chunk.css"
+  },
+  {
+    "revision": "6444216ad938f961badf",
+    "url": "/goit-react-hw-1-components/static/js/2.1dd7c9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-1-components/static/js/2.1dd7c9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78fe0d596fb8da2fb3b0",
+    "url": "/goit-react-hw-1-components/static/js/main.d3215361.chunk.js"
+  },
+  {
+    "revision": "336c1e88d7ec62345390",
+    "url": "/goit-react-hw-1-components/static/js/runtime-main.8d40f6d7.js"
+  }
+]);
